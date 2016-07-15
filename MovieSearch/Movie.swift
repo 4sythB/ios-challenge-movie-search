61d38eb424af67f00bd3da636d3c6c1e5ecbe1ee
Brad Forsyth <b4syth@gmail.com>
2016-07-15 09:41:41 -0600

    Created the movie model.

--- MovieSearch/Movie.swift
+++ MovieSearch/Movie.swift
@@ -10,5 +10,32 @@ import Foundation
 
 class Movie {
     
+    private let kTitle = "title"
+    private let kRating = "vote_average"
+    private let kOverview = "overview"
+    private let kPoster = "poster_path"
     
+    var title: String
+    var rating: Int
+    var overview: String
+    var poster: String
+    
+    init(title: String, rating: Int, overview: String, poster: String) {
+        self.title = title
+        self.rating = rating
+        self.overview = overview
+        self.poster = poster
+    }
+    
+    init?(dictionary: [String : AnyObject]) {
+        guard let title = dictionary[kTitle] as? String,
+            rating = dictionary[kRating] as? Int,
+            overview = dictionary[kOverview] as? String,
+            poster = dictionary[kPoster] as? String else { return nil }
+        
+        self.title = title
+        self.rating = rating
+        self.overview = overview
+        self.poster = poster
+    }
 }
