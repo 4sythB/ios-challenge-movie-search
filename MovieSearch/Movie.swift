afa382ecf187f7072d00fac72bcec1ed6ac9e0a1
Brad Forsyth <b4syth@gmail.com>
2016-07-15 09:23:54 -0600

    Set up table view in storyboard. Added network controller and other files.

---
+++ MovieSearch/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Brad on 7/15/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+    
+}
---
+++ MovieSearch/MovieController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Brad on 7/15/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
